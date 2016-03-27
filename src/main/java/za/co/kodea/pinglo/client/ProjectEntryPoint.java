d0eaaab2d13f8e0f97b8f054ca466cfd1a480aa5
Thato Nthite <tt.nthite@gmail.com>
2016-03-27 22:15:30 +0200

    Got the GMap up

--- src/main/java/za/co/kodea/pinglo/client/ProjectEntryPoint.java
+++ src/main/java/za/co/kodea/pinglo/client/ProjectEntryPoint.java
@@ -1,12 +1,13 @@
 package za.co.kodea.pinglo.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.maps.client.LoadApi;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.sencha.gxt.core.client.GXT;
-import com.sencha.gxt.widget.core.client.box.MessageBox;
-import com.sencha.gxt.widget.core.client.button.TextButton;
-import com.sencha.gxt.widget.core.client.event.SelectEvent;
-import com.sencha.gxt.widget.core.client.event.SelectEvent.SelectHandler;
+import com.google.gwt.user.client.ui.Widget;
+import za.co.kodea.pinglo.client.view.TransitDirectionsServiceMapWidget;
+
+import java.util.ArrayList;
+
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -15,16 +16,43 @@ public class ProjectEntryPoint implements EntryPoint {
 
   @Override
   public void onModuleLoad() {
-    String version = GXT.getVersion().getRelease();
-    TextButton textButton = new TextButton("Verify GXT Works: Version=" + version);
-    RootPanel.get().add(textButton);
-    textButton.addSelectHandler(new SelectHandler() {
-      @Override
-      public void onSelect(SelectEvent event) {
-        MessageBox messageBox = new MessageBox("GXT Works.");
-        messageBox.show();
-      }
-    });
+    loadMapApi();
   }
-  
+
+  private void loadMapApi() {
+    boolean sensor = true;
+
+    // load all the libs for use in the maps
+    ArrayList<LoadApi.LoadLibrary> loadLibraries = new ArrayList<>();
+    loadLibraries.add(LoadApi.LoadLibrary.ADSENSE);
+    loadLibraries.add(LoadApi.LoadLibrary.DRAWING);
+    loadLibraries.add(LoadApi.LoadLibrary.GEOMETRY);
+    loadLibraries.add(LoadApi.LoadLibrary.PANORAMIO);
+    loadLibraries.add(LoadApi.LoadLibrary.PLACES);
+    loadLibraries.add(LoadApi.LoadLibrary.WEATHER);
+    loadLibraries.add(LoadApi.LoadLibrary.VISUALIZATION);
+
+    Runnable onLoad = new Runnable() {
+      @Override
+      public void run() {
+        draw();
+      }
+    };
+
+    LoadApi.go(onLoad, loadLibraries, sensor);
+  }
+
+  private void draw() {
+    drawTransitDirections();
+  }
+
+  private void drawTransitDirections() {
+    TransitDirectionsServiceMapWidget wMap = new TransitDirectionsServiceMapWidget();
+    addMapWidget(wMap);
+  }
+
+  private void addMapWidget(Widget widget) {
+    RootPanel.get().add(widget);
+  }
+
 }
